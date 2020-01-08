2ad9d37f047d520e62eda52ada6ddbb24c4d53ad
Raymond Korir <raymondkipngetichfx@gmail.com>
2020-01-08 19:09:48 +0300

    Update and rename Solution.py to Solution.clj

---
+++ Solution.clj
@@ -0,0 +1,24 @@
+
+; Complete the bonAppetit function below.
+(defn bonAppetit [bill k b]
+   (let [not-pay (nth bill k)
+        total    (reduce + bill)
+        b-actual (/ (- total not-pay) 2)]
+    (println
+      (if (= b b-actual)
+        "Bon Appetit"
+        (- b b-actual))))
+
+)
+
+(def nk (clojure.string/split (clojure.string/trimr (read-line)) #" "))
+
+(def n (Integer/parseInt (nth nk 0)))
+
+(def k (Integer/parseInt (nth nk 1)))
+
+(def bill (vec (map #(Integer/parseInt %) (clojure.string/split (clojure.string/trimr (read-line)) #" "))))
+
+(def b (Integer/parseInt (clojure.string/trim (read-line))))
+
+(bonAppetit bill k b)
